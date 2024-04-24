f42874942cd3590fc32116980e1b999720022856
Emii-lia <fiaromiangaly@gmail.com>
2024-04-24 11:38:12 +0300

    add: Added react-query provider and hooks

--- src/provider/reactQuery.provider.tsx
+++ src/provider/reactQuery.provider.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { ReactNode} from "react";
 import {QueryClientProvider} from "react-query";
-import {queryClient} from "@/lib/queryClient";
+import { queryClient } from '../lib/queryClient.ts';
 
 export default function ReactQueryProvider({children}:{children:ReactNode}) {
     return(
